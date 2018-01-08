7db567084d57ff97e12324ffaec216b6a1d8202f
Abhishek Kumar Singh <kumar.abh494@gmail.com>
2018-01-08 20:55:22 +0530

    Version 1.0.0

---
+++ src/notepad/Notepad.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package notepad;
+
+/**
+ *
+ * @author Abhishek Kumar Singh & Surbhit Awasthi
+ *       a.k.a BINARY WARRIORS
+ */
+public class Notepad {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        pad p=new pad();
+                p.setVisible(true);
+    }
+    
+}
